5b1327a43dbbdb25b53e534f77d663e07676fe14
Farooq <farooq@techservicehub.io>
2025-11-11 21:49:13 +0100

    [PAYS-141] Unable to configure tiered custom fees for clients — only single amount allowed

--- custom/paystack/infrastructure/event/src/test/java/com/paystack/fineract/infrastructure/event/external/service/KafkaNotificationReadPlatformServiceImplTest.java
+++ custom/paystack/infrastructure/event/src/test/java/com/paystack/fineract/infrastructure/event/external/service/KafkaNotificationReadPlatformServiceImplTest.java
@@ -18,8 +18,8 @@
  */
 package com.paystack.fineract.infrastructure.event.external.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
