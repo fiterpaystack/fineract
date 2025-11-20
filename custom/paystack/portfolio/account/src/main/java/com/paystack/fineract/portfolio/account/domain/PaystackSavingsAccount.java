2e589860e30c8f5f414a87a526ec036d9edb7d1b
Farooq <farooq@techservicehub.io>
2025-11-20 06:31:44 +0100

    [PAYS-144]Allow specifying a custom Account Name when creating a Savings Account

--- custom/paystack/portfolio/account/src/main/java/com/paystack/fineract/portfolio/account/domain/PaystackSavingsAccount.java
+++ custom/paystack/portfolio/account/src/main/java/com/paystack/fineract/portfolio/account/domain/PaystackSavingsAccount.java
@@ -19,14 +19,8 @@
 
 package com.paystack.fineract.portfolio.account.domain;
 
-import static org.apache.fineract.infrastructure.core.domain.AuditableFieldsConstants.CREATED_BY_DB_FIELD;
-import static org.apache.fineract.infrastructure.core.domain.AuditableFieldsConstants.CREATED_DATE_DB_FIELD;
-import static org.apache.fineract.infrastructure.core.domain.AuditableFieldsConstants.LAST_MODIFIED_BY_DB_FIELD;
-import static org.apache.fineract.infrastructure.core.domain.AuditableFieldsConstants.LAST_MODIFIED_DATE_DB_FIELD;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -34,18 +28,16 @@ import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
-import java.time.OffsetDateTime;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.fineract.infrastructure.core.domain.AbstractAuditableWithUTCDateTimeCustom;
 import org.apache.fineract.portfolio.savings.domain.SavingsAccount;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Table(name = "ps_m_savings_account")
-@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
-public class PaystackSavingsAccount {
+public class PaystackSavingsAccount extends AbstractAuditableWithUTCDateTimeCustom<Long> {
 
     @Id
     @Column(name = "savings_account_id")
@@ -61,17 +53,4 @@ public class PaystackSavingsAccount {
     @MapsId
     @JoinColumn(name = "savings_account_id")
     private SavingsAccount savingsAccount;
-
-    @Column(name = CREATED_BY_DB_FIELD, updatable = false, nullable = false)
-    private Long createdBy;
-
-    @Column(name = CREATED_DATE_DB_FIELD, updatable = false, nullable = false)
-    private OffsetDateTime createdDate;
-
-    @Column(name = LAST_MODIFIED_BY_DB_FIELD, nullable = false)
-    private Long lastModifiedBy;
-
-    @Column(name = LAST_MODIFIED_DATE_DB_FIELD, nullable = false)
-    private OffsetDateTime lastModifiedDate;
-
 }
