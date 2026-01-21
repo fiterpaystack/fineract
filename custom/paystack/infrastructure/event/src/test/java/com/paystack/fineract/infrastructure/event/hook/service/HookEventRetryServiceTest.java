81137d9602cd311b4f34e7d2f24bf2496ddaf6a7
Farooq <farooq@fiter.io>
2026-01-21 06:55:40 +0100

    [FSP-2] Replace Default Fineract Webhooks with Kafka-Based Notification Architecture

--- custom/paystack/infrastructure/event/src/test/java/com/paystack/fineract/infrastructure/event/hook/service/HookEventRetryServiceTest.java
+++ custom/paystack/infrastructure/event/src/test/java/com/paystack/fineract/infrastructure/event/hook/service/HookEventRetryServiceTest.java
@@ -26,7 +26,6 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
